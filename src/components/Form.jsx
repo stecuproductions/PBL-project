4b34e3e6b4bbd4beffb2ba622e92a5bdff41e831
stecuproductions <stecu03@gmail.com>
2025-01-07 22:18:45 +0100

    wysylanie maili dziala

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,50 @@
-import React from "react";
-import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
-function Form(){
-    return(
-        <div className="form-container">
+function Form() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_88lb60s", // Zamień na ID usługi z EmailJS
+        "template_0zvz6an", // Zamień na ID szablonu z EmailJS
+        form.current,
+        "VIfelb9ZMKewGh4fw" // Zamień na publiczny klucz API z EmailJS
+      )
+      .then(
+        (result) => {
+          alert("Wiadomość wysłana pomyślnie!");
+        },
+        (error) => {
+          alert("Wystąpił błąd podczas wysyłania wiadomości.");
+        }
+      );
+  };
+
+  return (
+    <div className="form-container">
       <h1>Formularz zgłoszeniowy</h1>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <div className="form-group">
-          <label htmlFor="name">Imię:</label>
+          <label htmlFor="name">Imię (opcjonalne):</label>
           <input
             type="text"
             id="name"
             name="name"
             placeholder="Wpisz swoje imię"
-            required
           />
         </div>
 
         <div className="form-group">
-          <label htmlFor="email">E-mail:</label>
+          <label htmlFor="email">E-mail (opcjonalny):</label>
           <input
             type="email"
             id="email"
             name="email"
-            placeholder="Wpisz swój e-mail (opcjonalnie)"
-            required
+            placeholder="Wpisz swój e-mail"
           />
         </div>
 
@@ -44,7 +64,7 @@ function Form(){
         </button>
       </form>
     </div>
-    );
+  );
 }
 
 export default Form;
